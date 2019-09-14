0865238f8d5261e654bce2e287fd44af52c921fe
Karol Sobański <sobanskikarol91@gmail.com>
2019-09-14 16:15:33 +0200

    Fix: changing color

--- Fifty-Fifty/Assets/Scripts/HairController.cs
+++ Fifty-Fifty/Assets/Scripts/HairController.cs
@@ -1,22 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HairController : MonoBehaviour
 {
-    [SerializeField] GameObject blueHair;
     [SerializeField] GameObject greenHair;
+    [SerializeField] GameObject blueHair;
     [SerializeField] GameObject redHair;
     GameObject currentHair;
 
+    HairColor currentColor;
+
+
     private void Awake()
     {
+        currentColor = HairColor.Green;
         currentHair = greenHair;
+        DisableOldParticles(blueHair);
+        DisableOldParticles(redHair);
     }
 
     public void ChangeHairColor(HairColor color)
     {
-        currentHair.SetActive(false);
+        if (currentColor == color) return;
+        currentColor = color;
+
+        DisableOldParticles(currentHair);
 
         if (color == HairColor.Blue)
             currentHair = blueHair;
@@ -25,6 +35,18 @@ public class HairController : MonoBehaviour
         else if (color == HairColor.Red)
             currentHair = redHair;
 
-        currentHair.SetActive(true);
+        EnableNewParticles(currentHair);
+    }
+
+    private void EnableNewParticles(GameObject holder)
+    {
+        ParticleSystem[] particles = holder.GetComponentsInChildren<ParticleSystem>();
+        Array.ForEach(particles, p => p.enableEmission = true);
+    }
+
+    private void DisableOldParticles(GameObject holder)
+    {
+        ParticleSystem[] particles = holder.GetComponentsInChildren<ParticleSystem>();
+        Array.ForEach(particles, p => p.enableEmission = false);
     }
 }
