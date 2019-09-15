72afdf9fe4753639f7824035e59d164e50075fd5
Karol Sobański <sobanskikarol91@gmail.com>
2019-09-15 08:52:23 +0200

    Repaired Menu

--- Fifty-Fifty/Assets/Scripts/GameManager.cs
+++ Fifty-Fifty/Assets/Scripts/GameManager.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public Transform Player;
     public static GameManager instance;
-
+    public GameObject gameOver;
     private MenuController menu;
 
     private void Awake()
@@ -17,6 +18,16 @@ public class GameManager : MonoBehaviour
 
     internal void GameOver()
     {
-        menu.GameOver();
+        Invoke("GameOverPanel", 1.6f);
+    }
+
+    private void GameOverPanel()
+    {
+        gameOver.SetActive(true);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene("Game");
     }
 }
